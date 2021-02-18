3fe41784c1fb2808903a10809dcc6d532588d7b2
hupe13 <73023225+hupe13@users.noreply.github.com>
2021-02-18 13:07:49 +0100

    Bump version of fullscreen plugin to v2.0.0

--- php/fullscreen.php
+++ php/fullscreen.php
@@ -4,10 +4,10 @@ function fc_function(){
 	global $post;
 	if ( ! is_page() ) return;
 	wp_enqueue_style( 'leaflet.fullscreen',
-		'https://unpkg.com/leaflet.fullscreen@1.6.0/Control.FullScreen.css',
+		'https://unpkg.com/leaflet.fullscreen@2.0.0/Control.FullScreen.css',
 		array('leaflet_stylesheet'));
 	wp_enqueue_script('leaflet.fullscreen',
-		'https://unpkg.com/leaflet.fullscreen@1.6.0/Control.FullScreen.js',
+		'https://unpkg.com/leaflet.fullscreen@2.0.0/Control.FullScreen.js',
 		array('leaflet_js'));
 	// my
 	wp_enqueue_script('myfullscreen',
