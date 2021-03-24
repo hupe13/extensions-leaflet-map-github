2d109e288a034171b80c365e82968a6a20f783c9
hupe13 <73023225+hupe13@users.noreply.github.com>
2021-03-24 17:51:55 +0100

    Update wp-leaflet-extensions.php

--- wp-leaflet-extensions.php
+++ wp-leaflet-extensions.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Extensions for Leaflet Map
  * Description: Extensions of the Wordpress plugin Leaflet Map
- * Version: 0.0.5
+ * Version: 0.0.55
  * Author: hupe13
  * Plugin URI: https://wordpress.org/plugins/extensions-leaflet-map/
 **/
