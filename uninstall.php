f970ee205f4e97299fac27f6d292edafab9d16b9
hupe13 <hupe13@phw-web.de>
2021-04-09 20:22:47 +0200

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -5,7 +5,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 }
 
 $option_names = array ('leafext_values', 'leafext_maps');
-foreach ( $otion_names as $option_name ) {
+foreach ( $option_names as $option_name ) {
 	delete_option($option_name);
 	// for site options in Multisite
 	delete_site_option($option_name);
