fa13d8c9b12f8583427cade975b84deced484512
hupe13 <hupe13@phw-web.de>
2021-04-16 20:49:17 +0200

    popup on hover

--- js/hovergeojson.js
+++ js/hovergeojson.js
@@ -1,10 +1,14 @@
 // For use with only one map on a webpage
+
 window.WPLeafletMapPlugin = window.WPLeafletMapPlugin || [];
 window.WPLeafletMapPlugin.push(function () {
 	var map = window.WPLeafletMapPlugin.getCurrentMap();
+	//console.log(map);
+	//console.log(window.WPLeafletMapPlugin);
 	if ( WPLeafletMapPlugin.geojsons.length > 0 ) {
 		var geojsons = window.WPLeafletMapPlugin.geojsons;
 		var geocount = geojsons.length;
+
 		for (var j = 0, len = geocount; j < len; j++) {
 			var geojson = geojsons[j];
 			//console.log(geojson);
@@ -23,6 +27,24 @@ window.WPLeafletMapPlugin.push(function () {
 					weight: 3
 				});
 			});
+			geojson.layer.on('mouseover', function (e) {
+				e.target.eachLayer(function(layer) {
+					layer.openPopup();
+				});
+			});
+			geojson.layer.on('mousemove', function (e) {
+				e.target.eachLayer(function(layer) {
+					layer.getPopup().setLatLng(e.latlng);
+				});
+            });
+			//Klappt irgendwie nicht, arbeitet nicht sauber
+			// geojson.layer.on('mouseout', function (e) {
+				// //console.log(e);
+				// e.target.eachLayer(function(layer) {
+					// //console.log(layer.isPopupOpen());
+					// layer.closePopup();
+				// });
+			// });
 		}
 	}
 });
