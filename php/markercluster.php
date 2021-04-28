f6519a56a145f74dec442a97c43e409a78b5c6e1
hupe13 <hupe13@users.noreply.github.com>
2021-04-28 13:39:07 +0200

    Update markercluster.php

--- php/markercluster.php
+++ php/markercluster.php
@@ -2,7 +2,7 @@
 //Shortcode: [cluster]
 // For use with only one map on a webpage
 // For use with more than one map on a webpage
-function leafext_cluster_function(){
+function leafext_cluster_function( $atts ){
 	wp_enqueue_style( 'markercluster.default',
 		plugins_url('leaflet-plugins/leaflet.markercluster-1.5.0/css/MarkerCluster.Default.min.css',LEAFEXT_PLUGIN_FILE),
 		array('leaflet_stylesheet'),null);
@@ -13,8 +13,23 @@ function leafext_cluster_function(){
 		plugins_url('leaflet-plugins/leaflet.markercluster-1.5.0/js/leaflet.markercluster.js',LEAFEXT_PLUGIN_FILE),
 		array('wp_leaflet_map'),null );
 	// custom js
+	// wp_enqueue_script('my_markercluster',
+	// 	plugins_url('js/markercluster.min.js',LEAFEXT_PLUGIN_FILE), array('markercluster'),null);
 	wp_enqueue_script('my_markercluster',
-		plugins_url('js/markercluster.min.js',LEAFEXT_PLUGIN_FILE), array('markercluster'),null);
+		plugins_url('js/markercluster.min.js',LEAFEXT_PLUGIN_FILE),
+		array('wp_leaflet_map'), null);
+
+	$defaults = get_option('leafext_cluster');
+	if ( ! $defaults ) $defaults = array();
+	//var_dump($defaults);
+	if (!array_key_exists('zoom', $defaults)) $defaults['zoom'] = "17";
+	if (!array_key_exists('radius', $defaults)) $defaults['radius'] = "50";
+
+	$cluster_options = shortcode_atts( array('zoom' => $defaults['zoom'], 'radius' => $defaults['radius']), $atts);
+	//var_dump($cluster_options); wp_die();
+
+	// Uebergabe der php Variablen an Javascript
+	wp_localize_script( 'my_markercluster', 'cluster', $cluster_options);
 }
 add_shortcode('cluster', 'leafext_cluster_function' );
 ?>
