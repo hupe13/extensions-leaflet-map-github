c23cdd56299b0089931f8086f1863c0b2170b901
hupe13 <hupe13@phw-web.de>
2021-04-30 21:41:08 +0200

    more help

--- admin/help/markercluster.php
+++ admin/help/markercluster.php
@@ -2,7 +2,7 @@
 $text=$text.'<h4 id="leaflet.markercluster">Leaflet.markercluster</h4>
 <img src="'.$leafext_picts.'cluster.png">
 <p>'.__('Many markers on a map become confusing. That is why they are clustered','extensions-leaflet-map').'.</p>'.__('
-You can define radius (maxClusterRadius) and zoom (disableClusteringAtZoom) in <a href="admin.php?page=extensions-leaflet-map&tab=cluster">Settings</a> or per map.'
+You can define some options in <a href="admin.php?page=extensions-leaflet-map&tab=cluster">Settings</a> or per map.'
 ,'extensions-leaflet-map').
 '<pre><code>[leaflet-map ....]
 // many markers
@@ -12,6 +12,6 @@ You can define radius (maxClusterRadius) and zoom (disableClusteringAtZoom) in <
 [leaflet-marker lat=... lng=... ...]poixx[/leaflet-marker]
 [cluster]
 // or
-[cluster radius="..." zoom="..."]
+[cluster radius="..." zoom="..." spiderfy=0]
 [zoomhomemap]
 </code></pre>';
