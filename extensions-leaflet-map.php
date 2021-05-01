84b9dc4b2977f3c3461cacafc44472d815862cad
hupe13 <73023225+hupe13@users.noreply.github.com>
2021-05-01 09:28:18 +0200

    Update extensions-leaflet-map.php

--- extensions-leaflet-map.php
+++ extensions-leaflet-map.php
@@ -55,7 +55,7 @@ if (is_admin()) {
 // Add settings to plugin page
 function leafext_add_action_links ( $actions ) {
 	$setting_page = dirname( plugin_basename( __FILE__ ) );
-	$actions[] = '<a href="'. esc_url( get_admin_url(null, 'admin.php?page='.$settings_page) ) .'">'. esc_html__( "Settings").'</a>';
+	$actions[] = '<a href="'. esc_url( get_admin_url(null, 'admin.php?page='.$setting_page) ) .'">'. esc_html__( "Settings").'</a>';
   return $actions;
 }
 add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'leafext_add_action_links' );
