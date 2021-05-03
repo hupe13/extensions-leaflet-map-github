2d494414310f084c803193fe74c8762360b83cc5
hupe13 <hupe13@users.noreply.github.com>
2021-05-03 13:30:59 +0200

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -4,9 +4,11 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
-$option_names = array ('leafext_values', 'leafext_maps');
-foreach ( $option_names as $option_name ) {
-	delete_option($option_name);
-	// for site options in Multisite
-	delete_site_option($option_name);
+global $wpdb;
+$option_names = $wpdb->get_results( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE 'leafext_%' " );
+foreach ( $option_names as $key=>$value ) {
+  delete_option($value->option_name);
+  // for site options in Multisite
+  delete_site_option($value->option_name);
 }
+?>
