e453f9fc3acb44427877ff8a09c0501189999188
hupe13 <hupe13@users.noreply.github.com>
2021-05-07 15:25:49 +0200

    gesture

--- php/gesture.php
+++ php/gesture.php
@@ -1,21 +1,45 @@
 <?php
 // For use with any map on a webpage
-function leafext_gestures_leaflet_loaded() {
+
+function leafext_gestures_script(){
+	include_once LEAFEXT_PLUGIN_DIR . '/pkg/JShrink/Minifier.php';
+	$text = '
+	// For use with any map on a webpage
+	//GestureHandling disables the following map attributes.
+	//dragging
+	//tap
+	//scrollWheelZoom
+
+	(function() {
+		function main() {
+			var maps = window.WPLeafletMapPlugin.maps;
+			//console.log("gesture");
+			for (var i = 0, len = maps.length; i < len; i++) {
+				var map = maps[i];
+				if ( map.dragging.enabled()
+						|| map.scrollWheelZoom.enabled()
+					) {
+					//console.log("enabled");
+					map.gestureHandling.enable();
+				}
+			}
+		}
+		window.addEventListener("load", main);
+	})();
+	';
+$text = \JShrink\Minifier::minify($text);
+return "\n".$text."\n";
+}
+
+function leafext_gestures_function() {
 	wp_enqueue_script('gestures_leaflet',
 		plugins_url('leaflet-plugins/leaflet-gesture-handling-1.2.1/js/leaflet-gesture-handling.min.js',LEAFEXT_PLUGIN_FILE),
 		array('wp_leaflet_map'), null);
 	wp_enqueue_style('gestures_leaflet_styles',
 		plugins_url('leaflet-plugins/leaflet-gesture-handling-1.2.1/css/leaflet-gesture-handling.min.css',LEAFEXT_PLUGIN_FILE),
 		array('leaflet_stylesheet'),null);
-	// custom js
-	wp_enqueue_script('gestures_leaflet_custom', plugins_url('js/gesture.min.js',LEAFEXT_PLUGIN_FILE), array('gestures_leaflet'), null);
+	wp_add_inline_script( 'gestures_leaflet', leafext_gestures_script(), 'after' );
 }
+add_action( 'wp_enqueue_scripts', 'leafext_gestures_function' );
 
-// Enqueue if shortcode [leaflet-map] exists
-add_filter('pre_do_shortcode_tag', function ( $output, $shortcode ) {
-	if ( 'leaflet-map' == $shortcode ) {
-		leafext_gestures_leaflet_loaded();
-    }
-	return $output;
-}, 10, 2);
 ?>
