60cd0062c87fab786ddb9c95631ee6f30c3bb1c0
hupe13 <hupe13@phw-web.de>
2021-05-08 22:30:23 +0200

    Update gesture.php

--- admin/help/gesture.php
+++ admin/help/gesture.php
@@ -3,7 +3,7 @@ $text=$text.'<h4 id="gesturehandling">GestureHandling</h4>
 <img src="'.$leafext_picts.'gesture.png">
 <p>Brings the basic functionality of Gesture Handling into Leaflet Map.
 Prevents users from getting trapped on the map when scrolling a long page.
-You can enable it for all maps or for particular maps.It becomes active 
+You can enable it for all maps or for particular maps. It becomes active 
 only when dragging or scrollWheelZoom is enabled.</p>
 <p>You can define the options in 
 <a href="admin.php?page=extensions-leaflet-map&tab=gesture">Settings</a>.</p>';
