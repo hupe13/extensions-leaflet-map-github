9372c2328abbd7cfc61ec02c099d231b99683f5f
hupe13 <hupe13@phw-web.de>
2021-05-17 19:45:39 +0200

    Update zoomhome.php

--- php/zoomhome.php
+++ php/zoomhome.php
@@ -24,11 +24,11 @@ function leafext_zoomhome_script($fit){
 	window.WPLeafletMapPlugin = window.WPLeafletMapPlugin || [];
 	window.WPLeafletMapPlugin.push(function () {
 		var map = window.WPLeafletMapPlugin.getCurrentMap();
-    //console.log(window.WPLeafletMapPlugin);
-		console.log("map._leaflet_id "+map._leaflet_id);
+		//console.log(window.WPLeafletMapPlugin);
 		var map_id = map._leaflet_id;
-    var maps=[];
-    maps[map_id] = map;
+		var maps=[];
+		maps[map_id] = map;
+		console.log("map_id* "+map_id);
 		console.log("fit "+'.json_encode($fit).');
 
 		if(typeof maps[map_id].zoomControl !== "undefined") {
@@ -40,7 +40,7 @@ function leafext_zoomhome_script($fit){
 		var bounds = [];
 		bounds[map_id] = new L.latLngBounds();
 		var zoomHome = [];
-    zoomHome[map_id] = L.Control.zoomHome();
+		zoomHome[map_id] = L.Control.zoomHome();
 
 		maps[map_id].on("eledata_loaded", function(e) {
 			console.log("elevation loaded");
@@ -50,60 +50,57 @@ function leafext_zoomhome_script($fit){
 			zoom = -99;
 		});
 
-		//
-		var lines = window.WPLeafletMapPlugin.lines;
-		if (lines.length > 0) {
-			zoom++;
-			console.log("lines "+lines.length);
-			for (var k = 0, len = lines.length; k < len; k++) {
-				if (map_id == lines[k]._map._leaflet_id) {
-					var line = lines[k];
-					bounds[map_id].extend(line.getBounds());
-				}
-			}
-		}
-		//
-		var markers = window.WPLeafletMapPlugin.markers;
-		if (markers.length > 0) {
-			console.log("markers "+markers.length);
-			zoom++;
-			var markerArray = [];
-			markerArray[map_id] = [];
-
-			for (var m = 0, len = markers.length; m < len; m++) {
-				//console.log("marker "+m);
-				//console.log(markers[m]);
-				if ( markers[m]._map !== null ) {
-					//console.log(map_id, markers[m]._map._leaflet_id);
-					if (map_id == markers[m]._map._leaflet_id) {
-						markerArray[map_id].push(markers[m]);
-					  //console.log("valid");
-            //console.log(markers[m]);
-          }
-				}
-			}
-      console.log("markerArray.length "+markerArray[map_id].length);
-			var group = [];
-      group[map_id] = L.featureGroup(markerArray[map_id]);
-      //console.log(group[map_id].length);
-      if ( typeof group[map_id].length !== "undefined" ) {
-			     bounds[map_id].extend(group[map_id].getBounds());
-			     if ('.json_encode($fit).') maps[map_id].fitBounds(bounds[map_id]);
-      }
-		}
+		// //
+		// var lines = window.WPLeafletMapPlugin.lines;
+		// if (lines.length > 0) {
+			// console.log("lines "+lines.length);
+		// }
+		// //
+		// var markers = window.WPLeafletMapPlugin.markers;
+		// if (markers.length > 0) {
+			// console.log("markers "+markers.length);
+		// }
+		// //
+		// var circles = window.WPLeafletMapPlugin.circles;
+		// if (circles.length > 0) {
+			// console.log("circles "+circles.length);
+		// }
+		
 		//
 		var markergroups = window.WPLeafletMapPlugin.markergroups;
-    console.log("markergroups");
-    Object.entries(markergroups).forEach(([key, value]) => {
-      if ( markergroups[key]._map !== null ) {
-        if (map_id == markergroups[key]._map._leaflet_id) {
-          console.log("markergroups loop");
-          markergroups[key].eachLayer(function(layer) {
-            bounds[map_id].extend(layer._latlng);
-          });
-        }
-      }
-    });
+		var mapmarkers = 0;
+		var maplines = 0;
+		var mapcircles = 0;
+		Object.entries(markergroups).forEach(([key, value]) => {
+			if ( markergroups[key]._map !== null ) {
+				if (map_id == markergroups[key]._map._leaflet_id) {
+					//console.log("markergroups loop");
+					markergroups[key].eachLayer(function(layer) {
+						zoom++;
+						//console.log("looping "+zoom);
+						//console.log(layer);
+						if (layer instanceof L.Marker){
+							//console.log("is_marker");
+							mapmarkers++;
+							bounds[map_id].extend(layer._latlng);
+						} else if (layer instanceof L.Polyline){
+							//console.log("is_Line");
+							maplines++;
+							bounds[map_id].extend(layer._latlng);
+						} else if (layer instanceof L.Circle){
+							//console.log("is_Circle");
+							mapcircles++;
+							bounds[map_id].extend(layer._latlng);
+						} else {
+							console.log(layer);
+						}
+					});
+				}
+			}
+		});
+		console.log("markers "+mapmarkers);
+		console.log("lines "+maplines);
+		console.log("circles "+mapcircles);
 
 		//geojson asynchron
 		var geojsons = window.WPLeafletMapPlugin.geojsons;
@@ -128,18 +125,6 @@ function leafext_zoomhome_script($fit){
 		}
 
 		//
-		var circles = window.WPLeafletMapPlugin.circles;
-		if (circles.length > 0) {
-			console.log("circles "+circles.length);
-			zoom++;
-			for (var c = 0, len = circles.length; c < len; c++) {
-				if (map_id == circles[c]._map._leaflet_id) {
-					var circle = circles[c];
-					bounds[map_id].extend(circle.getBounds());
-				}
-			}
-		}
-		//
 		if ( zoom > 0 ) {
 			if (bounds[map_id].isValid()) {
 				zoomHome[map_id].addTo(map);
@@ -147,7 +132,7 @@ function leafext_zoomhome_script($fit){
 				maps[map_id].options.maxZoom = 19;
 				if ('.json_encode($fit).') {
 					//console.log("fit true");
-					console.log(maps[map_id].getZoom());
+					console.log("zoom "+maps[map_id].getZoom());
 					maps[map_id].fitBounds(bounds[map_id]);
 					//if (maps[map_id].getZoom() > 14 && zoom == 1) {
 						//	maps[map_id].setZoom(14);
@@ -195,6 +180,6 @@ function leafext_plugin_zoomhome_function($atts){
 	$params = shortcode_atts($defaults, $atts);
 	$params['fit'] = (bool)$params['fit'];
 
-  return leafext_zoomhome_script($params['fit']);
+	return leafext_zoomhome_script($params['fit']);
 }
 add_shortcode('zoomhomemap', 'leafext_plugin_zoomhome_function' );
