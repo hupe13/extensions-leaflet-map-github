12d5ec9e7a444cdebba848fb05675dc4689d5c98
hupe13 <hupe13@t-online.de>
2021-06-04 20:28:20 +0200

    safari.js

---
+++ js/safari.js
@@ -0,0 +1,23 @@
+(function() {
+  function main() {
+    var maps = window.WPLeafletMapPlugin.maps;
+    console.log("safaritest");
+    for (var i = 0, len = maps.length; i < len; i++) {
+      var map = maps[i];
+      if (typeof map.tap !== 'undefined') {
+        if (map.options.tap) {
+          map.tap.enable();
+        } else {
+          map.tap.disable();
+        }
+      }
+      var is_chrome = navigator.userAgent.indexOf("Chrome") > -1;
+      var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+      if ( !is_chrome && is_safari ) {
+		  console.log("is_safari");
+        map.tap.disable();
+      }
+    }
+  }
+  window.addEventListener("load", main);
+})();
