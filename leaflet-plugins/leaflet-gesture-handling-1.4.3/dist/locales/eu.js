e92864d40e6d34a5fc89878056127b81e3bba3f8
hupe13 <hupe13@t-online.de>
2022-06-06 22:59:05 +0200

    testen

--- leaflet-plugins/leaflet-gesture-handling-1.4.3/dist/locales/eu.js
+++
@@ -1,6 +0,0 @@
-//Basque
-export default {
-	touch: "Erabili bi hatz mapa mugitzeko",
-	scroll: "Mapan zooma aplikatzeko, sakatu Ktrl eta egin gora edo behera",
-	scrollMac: "Eduki sakatuta \u2318 eta egin gora eta behera mapa handitu eta txikitzeko"
-};
--- leaflet-plugins/leaflet-gesture-handling-1.4.3/dist/locales/kn.js
+++
@@ -1,6 +0,0 @@
-//Kannada
-export default {
-	touch: "Use two fingers to move the map",
-	scroll: "Use Ctrl + scroll to zoom the map",
-	scrollMac: "Use ⌘ + scroll to zoom the map"
-};
