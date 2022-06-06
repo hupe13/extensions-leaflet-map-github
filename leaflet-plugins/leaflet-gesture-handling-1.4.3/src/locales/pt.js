e71d2763ff17857f64972dd78a99e4ac9bcea9da
hupe13 <hupe13@t-online.de>
2022-06-06 23:38:01 +0200

    back

--- leaflet-plugins/leaflet-gesture-handling-1.4.3/src/locales/pt.js
+++
@@ -1,6 +0,0 @@
-//Portuguese
-export default {
-	touch: "Use dois dedos para mover o mapa",
-	scroll: "Pressione Ctrl e role a tela simultaneamente para aplicar zoom no mapa",
-	scrollMac: "Use \u2318 e role a tela simultaneamente para aplicar zoom no mapa"
-};
