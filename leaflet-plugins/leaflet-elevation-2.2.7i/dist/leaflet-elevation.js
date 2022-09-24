f1bcecc35e59997d44686345a7ee90dc78074d3a
hupe13 <hupe13@t-online.de>
2022-09-24 17:14:05 +0200

    zurück

--- leaflet-plugins/leaflet-elevation-2.2.7i/dist/leaflet-elevation.js
+++ leaflet-plugins/leaflet-elevation-2.2.7i/dist/leaflet-elevation.js
@@ -137,8 +137,6 @@
 	 * TODO: use generators instead? (ie. "yield")
 	 */
 	const iMax = (iVal, max = -Infinity) => (iVal > max ? iVal : max);
-	//const iMin = (iVal, min = +Infinity) => ((iVal && iVal < min) ? iVal : min);
-	//const iAvg = (iVal, avg = 0, idx = 1) => (iVal && idx > 1) ? (iVal + avg * (idx - 1)) / idx : avg ;
 	const iMin = (iVal, min = +Infinity) => ((iVal && iVal < min) ? iVal : min);
 	const iAvg = (iVal, avg = 0, idx = 1) => (iVal && idx > 1) ? (iVal + avg * (idx - 1)) / idx : avg ;
 	const iSum = (iVal, sum = 0) => iVal + sum;
